54859e4b5592da3527e9297ea41e5b5a7655f1c9
angel <sakharovsa@63pokupki.ru>
2024-02-27 10:34:08 +0400

    FIX

--- src/clear.ts
+++ src/clear.ts
@@ -10,8 +10,12 @@ async function run(){
     const dtClear10Day = mFormatDateTime(dayjs().subtract(10, 'day'));
 
     const aidQuery = await dbProxy('query').where('created_at', '<', dtClear10Day).limit(10000).pluck('id');
-    await dbProxy('query').whereIn('id', aidQuery).del()
-    console.log('Удалено записей обработанных спустя 10 дней:', aidQuery.length);
+    if(aidQuery?.length){
+        await dbProxy('query').whereIn('id', aidQuery).del()
+        console.log('Удалено записей обработанных спустя 10 дней:', aidQuery.length);
+    } else {
+        console.log('Нет записей для удаления');
+    }
 
     console.log('=========================');
     console.log('END');
