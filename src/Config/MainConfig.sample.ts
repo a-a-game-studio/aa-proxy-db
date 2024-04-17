219185b09fb16d156c677320d5ee1d17ffdc194c
angel <sakharovsa@63pokupki.ru>
2024-04-17 12:50:10 +0400

    FIX

--- src/Config/MainConfig.sample.ts
+++ src/Config/MainConfig.sample.ts
@@ -15,7 +15,7 @@ export const common = {
 export const option = {
     replication:true, // Сохранять ли логи запросов, делать ли репликацию БД
     log:true, // Включено ли логирование по данным
-    mergeOnConflict:true // Поддерживает ли БД при update и insert при дупликации строк их совмещение
+    mergeOnConflict:true, // Поддерживает ли БД при update и insert при дупликации строк их совмещение
     timeoutQuery:60000 // timeout выполнения запроса на уровне балансировщика
 }
 
